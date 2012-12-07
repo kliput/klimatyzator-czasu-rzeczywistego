82601bea37dd8d87860dcf6939870e019363a1de
Jakub Liput <jakub.liput@gmail.com>
2012-12-07 13:38:31 +0100

    Zalążek czujników

---
+++ DefaultComponent/DefaultConfig/Default/Default_pkgClass.java
@@ -0,0 +1,28 @@
+/*********************************************************************
+	Rhapsody	: 7.6.1
+	Login		: Kuba
+	Component	: DefaultComponent
+	Configuration 	: DefaultConfig
+	Model Element	: Default
+//!	Generated Date	: Fri, 7, Dec 2012 
+	File Path	: DefaultComponent/DefaultConfig/Default/Default_pkgClass.java
+*********************************************************************/
+
+package Default;
+
+
+//----------------------------------------------------------------------------
+// Default/Default_pkgClass.java                                                                  
+//----------------------------------------------------------------------------
+
+//## package Default 
+
+
+//## ignore 
+public class Default_pkgClass {
+    
+}
+/*********************************************************************
+	File Path	: DefaultComponent/DefaultConfig/Default/Default_pkgClass.java
+*********************************************************************/
+
